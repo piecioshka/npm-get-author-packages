569ea1ed9f8847e1d1e2c2372e2caa87e15e0e04
Piotr Kowalski <piecioshka@gmail.com>
2025-01-16 23:00:26 +0100

    Initialize project

---
+++ README.md
@@ -0,0 +1,51 @@
+# npm-get-author-packages
+
+[![cli-available](https://badgen.net/static/cli/available/?icon=terminal)](https://runkit.com/npm/npm-get-author-packages)
+[![node version](https://img.shields.io/node/v/npm-get-author-packages.svg)](https://www.npmjs.com/package/npm-get-author-packages)
+[![npm version](https://badge.fury.io/js/npm-get-author-packages.svg)](https://badge.fury.io/js/npm-get-author-packages)
+[![downloads count](https://img.shields.io/npm/dt/npm-get-author-packages.svg)](https://www.npmjs.com/package/npm-get-author-packages)
+[![size](https://packagephobia.com/badge?p=npm-get-author-packages)](https://packagephobia.com/result?p=npm-get-author-packages)
+[![license](https://img.shields.io/npm/l/npm-get-author-packages.svg)](https://piecioshka.mit-license.org)
+
+🔨 Display projects of user from npm registry
+
+> Give a ⭐️ if this project helped you!
+
+## Features
+
+- ✅ List all npm packages of author
+
+## CLI
+
+Installation:
+
+```bash
+npm install -g npm-get-author-packages
+```
+
+```bash
+npm-get-author-packages piecioshka
+```
+
+```bash
+Found 44 packages:
+- [2015-08-20T10:19:08.710Z] vanilla-javascript
+- [2015-10-07T14:57:45.900Z] eslint-config-piecioshka
+- [2015-10-13T22:15:34.840Z] export-eslint-config
+- [2015-10-23T15:20:51.102Z] complete-string  TS
+- [2015-10-25T14:34:14.459Z] underscore.assert
+# ...
+```
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />
+Feel free to check [issues page](https://github.com/piecioshka/npm-get-author-packages/issues/).
+
+## Related
+
+- [npm-list-author-packages](https://github.com/kgryte/npm-list-author-packages)
+
+## License
+
+[The MIT License](https://piecioshka.mit-license.org) @ 2025
---
+++ bin/cli.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+
+const user = process.argv[2];
+
+if (!user) {
+  console.error("Usage: npm-get-author-packages <username>");
+  process.exit(1);
+}
+
+const displayTypeScript = () => {
+  const blueBackgroundWhiteText = "\x1b[48;2;48;120;198;38;2;255;255;255m";
+  const resetStyle = "\x1b[0m";
+  return `${blueBackgroundWhiteText} TS ${resetStyle}`;
+};
+
+const template = ({ date, name, hasTypes }) =>
+  `- [${date.toISOString()}] ${name} ${hasTypes ? displayTypeScript() : ""}`;
+
+async function makeRequest(url) {
+  const response = await fetch(url);
+  return await response.json();
+}
+
+async function* fetchUserPackages(username) {
+  const maxSize = 250;
+  let from = 0;
+
+  while (true) {
+    const searchUrl = `https://registry.npmjs.org/-/v1/search?text=maintainer:${username}&from=${from}&size=${maxSize}`;
+    const data = await makeRequest(searchUrl);
+    const packages = data.objects.map((pkg) => pkg.package);
+
+    for (const pkg of packages) {
+      yield pkg;
+    }
+
+    const done = data.total <= from + maxSize;
+
+    if (!done) {
+      console.log(
+        `Fetched ${Math.min(data.total, from + maxSize)} packages...`,
+      );
+      from += maxSize;
+    } else {
+      break;
+    }
+  }
+}
+
+async function getUserPackages(username) {
+  const result = [];
+  const packages = fetchUserPackages(username);
+
+  for await (const package of packages) {
+    const packageName = package.name;
+    const packageUrl = `https://registry.npmjs.org/${packageName}`;
+    const packageData = await makeRequest(packageUrl);
+
+    const versions = Object.keys(packageData.time);
+    const createdAt = new Date(packageData.time[versions[0]]);
+
+    const latestVersion = packageData["dist-tags"].latest;
+    const hasTypes = packageData.versions[latestVersion].types;
+
+    result.push({
+      date: createdAt,
+      name: packageName,
+      hasTypes,
+    });
+  }
+
+  return result;
+}
+
+getUserPackages(user)
+  .then((packages) => packages.sort((a, b) => a.date - b.date))
+  .then((packages) => {
+    if (packages.length === 0) {
+      console.log("No packages found");
+      return;
+    }
+
+    console.log(`Found ${packages.length} packages:`);
+    packages.forEach((pkg) => console.log(template(pkg)));
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
